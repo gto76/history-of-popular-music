c6069d59a65ff0e9338f22a4d5e1a2870e12cfa6
Jure Šorn <sornjure@gmail.com>
2017-04-30 02:07:00 +0200

    Radius of circles set dynamicaly, cleanup

--- examples/example-test.js
+++ examples/example-test.js
@@ -1,58 +1,75 @@
 example();
 
 function example() {
+  var tasks = [
+    {"startDate":new Date(1922, 0, 0), "endDate":new Date(1956, 0, 0),
+     "taskName":"RnR", "status":"RUNNING",
+     "songs": [{"song":"Robert_Karpinski-Dla_O"}] },
+    {"startDate":new Date(1956, 0, 0), "endDate":new Date(1966, 0, 0),
+     "taskName":"RnR", "status":"KILLED", 
+     "songs": [{"song":"502"}] },
+    {"startDate":new Date(1966, 0, 0), "endDate":new Date(1976, 0, 0), 
+     "taskName":"RnR", "status":"FAILED"},
+    {"startDate":new Date(1976, 0, 0), "endDate":new Date(1986, 0, 0), 
+     "taskName":"RnR", "status":"SUCCEEDED"},
 
-var tasks = [
+    {"startDate":new Date(1958, 0, 0), "endDate":new Date(1966, 0, 0), 
+     "taskName":"Rock", "status":"RUNNING"},
+    {"startDate":new Date(1966, 0, 0), "endDate":new Date(1976, 0, 0), 
+     "taskName":"Rock", "status":"KILLED"},
+    {"startDate":new Date(1976, 0, 0), "endDate":new Date(1986, 0, 0), 
+     "taskName":"Rock", "status":"FAILED"},
+    {"startDate":new Date(1986, 0, 0), "endDate":new Date(1996, 0, 0), 
+     "taskName":"Rock", "status":"SUCCEEDED"},
 
-{"startDate":new Date(1922, 0, 0),"endDate":new Date(1956, 0, 0),"taskName":"RnR","status":"RUNNING", "songs": [{"song":"Robert_Karpinski-Dla_O", "r": 20}] },
-{"startDate":new Date(1956, 0, 0),"endDate":new Date(1966, 0, 0),"taskName":"RnR","status":"KILLED", "songs": [{"song":"502", "r": 20}] },
-{"startDate":new Date(1966, 0, 0),"endDate":new Date(1976, 0, 0),"taskName":"RnR","status":"FAILED"},
-{"startDate":new Date(1976, 0, 0),"endDate":new Date(1986, 0, 0),"taskName":"RnR","status":"SUCCEEDED"},
+    {"startDate":new Date(1968, 0, 0), "endDate":new Date(1976, 0, 0), 
+     "taskName":"Punk", "status":"RUNNING"},
+    {"startDate":new Date(1976, 0, 0), "endDate":new Date(1986, 0, 0), 
+     "taskName":"Punk", "status":"KILLED"},
+    {"startDate":new Date(1986, 0, 0), "endDate":new Date(1996, 0, 0), 
+     "taskName":"Punk", "status":"FAILED"},
+    {"startDate":new Date(1996, 0, 0), "endDate":new Date(2006, 0, 0), 
+     "taskName":"Punk", "status":"SUCCEEDED"},
 
-{"startDate":new Date(1958, 0, 0),"endDate":new Date(1966, 0, 0),"taskName":"Rock","status":"RUNNING"},
-{"startDate":new Date(1966, 0, 0),"endDate":new Date(1976, 0, 0),"taskName":"Rock","status":"KILLED"},
-{"startDate":new Date(1976, 0, 0),"endDate":new Date(1986, 0, 0),"taskName":"Rock","status":"FAILED"},
-{"startDate":new Date(1986, 0, 0),"endDate":new Date(1996, 0, 0),"taskName":"Rock","status":"SUCCEEDED"},
+    {"startDate":new Date(1948, 0, 0), "endDate":new Date(1986, 0, 0), 
+     "taskName":"Hip-Hop", "status":"RUNNING"},
+    {"startDate":new Date(1986, 0, 0), "endDate":new Date(1996, 0, 0), 
+     "taskName":"Hip-Hop", "status":"KILLED"},
+    {"startDate":new Date(1996, 0, 0), "endDate":new Date(2006, 0, 0), 
+     "taskName":"Hip-Hop", "status":"FAILED"},
+    {"startDate":new Date(2006, 0, 0), "endDate":new Date(2016, 0, 0), 
+     "taskName":"Hip-Hop", "status":"SUCCEEDED"},
 
-{"startDate":new Date(1968, 0, 0),"endDate":new Date(1976, 0, 0),"taskName":"Punk","status":"RUNNING"},
-{"startDate":new Date(1976, 0, 0),"endDate":new Date(1986, 0, 0),"taskName":"Punk","status":"KILLED"},
-{"startDate":new Date(1986, 0, 0),"endDate":new Date(1996, 0, 0),"taskName":"Punk","status":"FAILED"},
-{"startDate":new Date(1996, 0, 0),"endDate":new Date(2006, 0, 0),"taskName":"Punk","status":"SUCCEEDED"},
+    {"startDate":new Date(1988, 0, 0), "endDate":new Date(1996, 0, 0), 
+     "taskName":"EDM", "status":"RUNNING"},
+    {"startDate":new Date(1996, 0, 0), "endDate":new Date(2006, 0, 0), 
+     "taskName":"EDM", "status":"KILLED"},
+    {"startDate":new Date(2006, 0, 0), "endDate":new Date(2016, 0, 0), 
+     "taskName":"EDM", "status":"FAILED"},
+    {"startDate":new Date(2016, 0, 0), "endDate":new Date(2026, 0, 0), 
+     "taskName":"EDM", "status":"SUCCEEDED"},
+  ];
 
-{"startDate":new Date(1948, 0, 0),"endDate":new Date(1986, 0, 0),"taskName":"Hip-Hop","status":"RUNNING"},
-{"startDate":new Date(1986, 0, 0),"endDate":new Date(1996, 0, 0),"taskName":"Hip-Hop","status":"KILLED"},
-{"startDate":new Date(1996, 0, 0),"endDate":new Date(2006, 0, 0),"taskName":"Hip-Hop","status":"FAILED"},
-{"startDate":new Date(2006, 0, 0),"endDate":new Date(2016, 0, 0),"taskName":"Hip-Hop","status":"SUCCEEDED"},
-
-{"startDate":new Date(1988, 0, 0),"endDate":new Date(1996, 0, 0),"taskName":"EDM","status":"RUNNING"},
-{"startDate":new Date(1996, 0, 0),"endDate":new Date(2006, 0, 0),"taskName":"EDM","status":"KILLED"},
-{"startDate":new Date(2006, 0, 0),"endDate":new Date(2016, 0, 0),"taskName":"EDM","status":"FAILED"},
-{"startDate":new Date(2016, 0, 0),"endDate":new Date(2026, 0, 0),"taskName":"EDM","status":"SUCCEEDED"},
-
-];
-
-var taskStatus = {
+  var taskStatus = {
     "SUCCEEDED" : "bar-revival",
     "FAILED" : "bar-crisis",
     "RUNNING" : "bar-proto",
     "KILLED" : "bar-golden"
-};
+  };
 
-var taskNames = [ "RnR", "Rock", "Punk", "Hip-Hop", "EDM" ];
+  var taskNames = [ "RnR", "Rock", "Punk", "Hip-Hop", "EDM" ];
 
-tasks.sort(function(a, b) {
+  tasks.sort(function(a, b) {
     return a.endDate - b.endDate;
-});
-var maxDate = tasks[tasks.length - 1].endDate;
-tasks.sort(function(a, b) {
+  });
+  var maxDate = tasks[tasks.length - 1].endDate;
+  tasks.sort(function(a, b) {
     return a.startDate - b.startDate;
-});
-var minDate = tasks[0].startDate;
+  });
+  var minDate = tasks[0].startDate;
 
-var format = "%y";
+  var format = "%y";
 
-var gantt = d3.gantt().taskTypes(taskNames).taskStatus(taskStatus).tickFormat(format);
-gantt(tasks);
-
-};
-
+  var gantt = d3.gantt().taskTypes(taskNames).taskStatus(taskStatus).tickFormat(format);
+  gantt(tasks);
+}
--- examples/music-icons.js
+++ examples/music-icons.js
@@ -1,27 +1,7 @@
-// <!DOCTYPE html>
-// <html>
-// <head>
-//  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+/**
+ * Script for drawing circles, that play music when clicked on.
+ */
 
-//  <script src="lib/jquery-2.1.3.js"></script>
-//  <script src="lib/d3.v3.min.js"></script>
-//  <script src="lib/bootstrap.min.js"></script>
-//  <link rel="stylesheet" href="lib/bootstrap.min.css">
-//  <script src="lib/bootstrap-slider.js"></script>
-//  <link rel="stylesheet" href="lib/bootstrap-slider.css">
-
-//  <script src="data/jamendo-data.js"></script>
-
-//  <style>
-//    .overlay {
-//      fill: none;
-//      pointer-events: all;
-//    }
-//  </style>
-// </head>
-
-
-//////////////////////////////////////
 var DEBUG = true
 
 var RADIUS = 20
@@ -51,68 +31,21 @@ var crossfadeDuration = CROSSFADE_DURATION
 var width = getWidth()
 var height = getHeight() 
 
+var hSpace = x.rangeBand() / 4;
+var vSpace = (y(new Date(1996, 0, 0)) - y(new Date(1986, 0, 0))) / 4;
 
-//################
-//##### DATA #####
-//################
+var r = Math.min(hSpace, vSpace);
 
-// var numOfSongs = Object.keys(songs.songs).length
+//////////////////////////////////////
 
-// Each node has 4 bubles, each in one sector,
-// var data = d3.range(numOfSongs)
-//   .map(function() { 
-//     return {r: RADIUS}; 
-//   })
-  
-// data.forEach(function(d, i) { 
-//   // SET id
-//   var keys = Object.keys(songs.songs)
-//   var songIndex = keys[i%numOfSongs]
-//   d.id = songIndex.slice(1)
-//   d.x = 100
-//   d.y = 100
-// })
-
-//###############
-//##### SVG #####
-//###############
-
-// var groups = svg.selectAll(".chart")
-//         .data(tasks, keyFunction).enter()
-//         .append("g")
-
-// Music icons. 
-// groups.filter(function(d) { return ("idd" in d); })
-//     .append("circle")
-//     .attr("cx", 0)
-//     .attr("cy", 0)
-//     .attr("r", 15)
-//     .attr("fill", "blue")
-//     .attr("transform", function(d) {
-//       dHor = x(d.taskName) + x.rangeBand()/2
-//       dVer = y(d.startDate)
-//       return "translate(" + dHor + "," + dVer + ")"
-//     })
-
-//#################
-//##### NODES #####
-//#################
-
-// var nodes = svg.selectAll("g")
-//   .data(data)
-//     .enter().append("g")
-//   .on("click", function(d) { play(d) })
-//   .attr('id', function(d) { return 'name' + d.id })
-
-
-var nodes = d3.selectAll("g.era") //.enter()
+var nodes = d3.selectAll("g.era")
   .filter(function(d) { return ("songs" in d); })
   .append("g")
     .on("click", function(d) { play(d.songs[0]) })
     .attr('id', function(d) { return 'name' + d.songs[0].song })
     .attr("transform", function(d) {
-      dHor = x(d.taskName) + x.rangeBand()/2 - d.songs[0].r
-      dVer = y(d.startDate) - d.songs[0].r
+      dHor = x(d.taskName) + x.rangeBand()/2 - r
+      dVer = y(d.startDate) - r
       return "translate(" + dHor + "," + dVer + ")"
     })
 
@@ -120,17 +53,17 @@ var clipPath = nodes.append("clipPath")
   .attr("id", "cut-off-bottom")
 
 clipPath.append("circle")
-  .attr("cx", function(d) { return d.songs[0].r; })
-  .attr("cy", function(d) { return d.songs[0].r; })
-  .attr("r", function(d) { return d.songs[0].r; })
+  .attr("cx", r)
+  .attr("cy", r)
+  .attr("r", r)
 
 nodes.append("image")
   .attr("x", "0")
   .attr("y", "0")
   .attr("xlink:href", 
       function(d) { return "../JamendoDataset/"+ d.songs[0].song + ".jpg" })
-  .attr("height", function(d) { return (d.songs[0].r) * 2; })
-  .attr("width", function(d) { return (d.songs[0].r) * 2; })
+  .attr("height", r * 2)
+  .attr("width", r * 2)
   .attr("clip-path", "url(#cut-off-bottom)")
 
 //#####################
@@ -141,9 +74,8 @@ addPlayIcon(nodes)
 
 function addPlayIcon(ggg) {
   var s = TRIANGLE_SIZE_FACTOR
-  var r = RADIUS
 
-  // there are five points so that all corners are rendered nicely
+  // Rhere are five points so that all corners of triangle are rendered nicely.
   var triangle = [ 
       { "x": r - (r*0.55 * s), "y": r - (r*0.45 * s) }, 
       { "x": r + (r*0.75 * s), "y": r },
@@ -158,9 +90,9 @@ function addPlayIcon(ggg) {
     .interpolate("linear");
 
   var shade = ggg.append("circle")
-    .attr("cx", function(d) { return d.r; })
-    .attr("cy", function(d) { return d.r; })
-    .attr("r", function(d) { return d.r; })
+    .attr("cx", r)
+    .attr("cy", r)
+    .attr("r", r)
     .attr("fill", BACKGROUND_COLOR)
     .attr("fill-opacity","0.4")
 
@@ -190,9 +122,7 @@ function setPlayIconOpacity(id, opacity) {
 //##### AUDIO #####
 //#################
 
-// generateAudioElements()
-
-// last audio element that started playing and is still playing
+// Last audio element that started playing and is still playing.
 var playing = ""
 
 var nodes = d3.selectAll("g.era") //.enter()
@@ -202,7 +132,6 @@ var nodes = d3.selectAll("g.era") //.enter()
   });
 
 function generateAudioElement(id) {
-  // var id = id.slice(1),
   var track = new Audio();
   var audioElement = document.createElement("source");
 
@@ -246,30 +175,30 @@ function play(nodesData){
 }
 
 function startPlayback(audioEl) {
-  // update state
+  // Updates state.
   var id = audioEl.id.slice(5)
   playing = audioEl
   
-  // fade in
+  // Fades in.
   fadeIn(audioEl, FADEIN_DURATION)
 }
 
 function skipTo(audioEl) {
-  // update state
+  // Updates state.
   exPlaying = playing
   playing = audioEl
 
-  // fade out/in
+  // Fades out/in.
   fadeOut(exPlaying, crossfadeDuration)
   fadeIn(audioEl, crossfadeDuration) 
 }
 
 function stopPlayback(audioEl) {
-  // update state
+  // Updates state.
   exPlaying = playing
   playing = ""
 
-  // fade out
+  // Fades out.
   fadeOut(audioEl, FADEOUT_DURATION)
   if (exPlaying != "") { 
     fadeOut(exPlaying, FADEOUT_DURATION)
