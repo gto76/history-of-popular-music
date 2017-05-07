135a417099389d4888cdfe7b88ceb8a607e89ee3
Jure Šorn <sornjure@gmail.com>
2017-05-07 04:30:30 +0200

    Capitalized ganras

--- js/example-test.js
+++ js/example-test.js
@@ -3,7 +3,7 @@ example();
 function example() {
   var tasks = [
     {"startDate":new Date(1922, 0, 0), "endDate":new Date(1956, 0, 0),
-     "taskName":"RnR", "status":"RUNNING",
+     "taskName":"R&R", "status":"RUNNING",
      "songs": [{"title":"the_fives",
                 "date": new Date(1922, 0, 0),
                 "year": 1922,
@@ -25,13 +25,13 @@ function example() {
                   "lines": [{"text": "R&B, ROCKABILLY"}]}
                 ]},
     {"startDate":new Date(1956, 0, 0), "endDate":new Date(1966, 0, 0),
-     "taskName":"RnR", "status":"KILLED",
+     "taskName":"R&R", "status":"KILLED",
      "songs": [{"title":"rock_around_the_clock", 
                 "date": new Date(1956, 0, 0),
                 "year": 1955,
                 "text": ["Bill Haley", "Rock Around the Clock"]}]},
     {"startDate":new Date(1966, 0, 0), "endDate":new Date(1976, 0, 0), 
-     "taskName":"RnR", "status":"FAILED",
+     "taskName":"R&R", "status":"FAILED",
      "remarks": [{"top": new Date(1971, 0, 0),
                   "bottom": new Date(1976, 0, 0),
                   "topMargin": 0,
@@ -39,26 +39,26 @@ function example() {
                   "lines": [{"text": "GLAM ROCK"}]}
                 ]},
     {"startDate":new Date(1976, 0, 0), "endDate":new Date(1986, 0, 0), 
-     "taskName":"RnR", "status":"SUCCEEDED",
+     "taskName":"R&R", "status":"SUCCEEDED",
      "songs": [{"title":"hang_on_st_christopher", 
                 "date": new Date(1986, 0, 0),
                 "year": 1987,
                 "text": ["Tom Waits", "Hang on St. Christopher"]}]},
 
     {"startDate":new Date(1958, 0, 0), "endDate":new Date(1966, 0, 0), 
-     "taskName":"Rock", "status":"RUNNING",
+     "taskName":"ROCK", "status":"RUNNING",
      "songs": [{"title":"rumble", 
                 "date": new Date(1958, 0, 0),
                 "year": 1958,
                 "text": ["Link Wray", "Rumble"]}]},
     {"startDate":new Date(1966, 0, 0), "endDate":new Date(1976, 0, 0), 
-     "taskName":"Rock", "status":"KILLED",
+     "taskName":"ROCK", "status":"KILLED",
      "songs": [{"title":"satisfaction", 
                 "date": new Date(1966, 0, 0),
                 "year": 1965,
                 "text": ["The Rolling Stones", "(I Can't Get No) Satisfaction"]}]},
     {"startDate":new Date(1976, 0, 0), "endDate":new Date(1986, 0, 0), 
-     "taskName":"Rock", "status":"FAILED",
+     "taskName":"ROCK", "status":"FAILED",
      "remarks": [{"top": new Date(1981, 0, 0),
                   "bottom": new Date(1986, 0, 0),
                   "topMargin": 0,
@@ -66,26 +66,26 @@ function example() {
                   "lines": [{"text": "HAIR METAL"}]}
                 ]},
     {"startDate":new Date(1986, 0, 0), "endDate":new Date(1996, 0, 0), 
-     "taskName":"Rock", "status":"SUCCEEDED",
+     "taskName":"ROCK", "status":"SUCCEEDED",
      "songs": [{"title":"devils_haircut", 
                 "date": new Date(1996, 0, 0),
                 "year": 1996,
                 "text": ["Beck", "Devils Haircut"]}]},
 
     {"startDate":new Date(1968, 0, 0), "endDate":new Date(1976, 0, 0), 
-     "taskName":"Punk", "status":"RUNNING",
+     "taskName":"PUNK", "status":"RUNNING",
      "songs": [{"title":"your_dog", 
                 "date": new Date(1968, 0, 0),
                 "year": 1969,
                 "text": ["The Stooges", "I Wanna Be Your Dog"]}]},
     {"startDate":new Date(1976, 0, 0), "endDate":new Date(1986, 0, 0), 
-     "taskName":"Punk", "status":"KILLED",
+     "taskName":"PUNK", "status":"KILLED",
      "songs": [{"title":"god_save_the_queen", 
                 "date": new Date(1976, 0, 0),
                 "year": 1977,
                 "text": ["Sex Pistols", "God Save the Queen"]}]},
     {"startDate":new Date(1986, 0, 0), "endDate":new Date(1996, 0, 0), 
-     "taskName":"Punk", "status":"FAILED",
+     "taskName":"PUNK", "status":"FAILED",
      "remarks": [{"top": new Date(1991, 0, 0),
                   "bottom": new Date(1996, 0, 0),
                   "topMargin": 0,
@@ -93,14 +93,14 @@ function example() {
                   "lines": [{"text": "SKATE PUNK"}]}
                 ]},
     {"startDate":new Date(1996, 0, 0), "endDate":new Date(2006, 0, 0), 
-     "taskName":"Punk", "status":"SUCCEEDED",
+     "taskName":"PUNK", "status":"SUCCEEDED",
      "songs": [{"title":"time_to_get_away", 
                 "date": new Date(2006, 0, 0),
                 "year": 2007,
                 "text": ["LCD Soundsystem", "Time to Get Away"]}]},
 
     {"startDate":new Date(1949, 0, 0), "endDate":new Date(1986, 0, 0), 
-     "taskName":"Hip-Hop", "status":"RUNNING",
+     "taskName":"HIP-HOP", "status":"RUNNING",
      "songs": [{"title":"mardi_gras_in_new_orleans", 
                 "date": new Date(1949, 0, 0),
                 "year": 1949,
@@ -116,13 +116,13 @@ function example() {
                   "lines": [{"text": "R&B"}, {"text": "FUNK"}, {"text": "REGGAE"}, {"text": "DISCO"}]}
                 ]},
     {"startDate":new Date(1986, 0, 0), "endDate":new Date(1996, 0, 0), 
-     "taskName":"Hip-Hop", "status":"KILLED",
+     "taskName":"HIP-HOP", "status":"KILLED",
      "songs": [{"title":"walk_this_way", 
                 "date": new Date(1986, 0, 0),
                 "year": 1986,
                 "text": ["Run-D.M.C", "Walk This Way"]}]},
     {"startDate":new Date(1996, 0, 0), "endDate":new Date(2006, 0, 0), 
-     "taskName":"Hip-Hop", "status":"FAILED",
+     "taskName":"HIP-HOP", "status":"FAILED",
      "remarks": [{"top": new Date(2001, 0, 0),
                   "bottom": new Date(2006, 0, 0),
                   "topMargin": 0,
@@ -130,7 +130,7 @@ function example() {
                   "lines": [{"text": "BLING RAP"}]}
                 ]},
     {"startDate":new Date(2006, 0, 0), "endDate":new Date(2016, 0, 0), 
-     "taskName":"Hip-Hop", "status":"SUCCEEDED",
+     "taskName":"HIP-HOP", "status":"SUCCEEDED",
      "songs": [{"title":"hood_politics", 
                 "date": new Date(2016, 0, 0),
                 "year": 2015,
@@ -167,7 +167,7 @@ function example() {
     "KILLED" : "bar-golden"
   };
 
-  var taskNames = [ "RnR", "Rock", "Punk", "Hip-Hop", "EDM" ];
+  var taskNames = [ "R&R", "ROCK", "PUNK", "HIP-HOP", "EDM" ];
 
   tasks.sort(function(a, b) {
     return a.endDate - b.endDate;
